2c4a1937362ce6cb90cb5e48255f78da0c98f7a1
Bemon <tristan2107b@gmail.com>
2023-08-14 06:08:16 +1000

    Implemented main JS logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+
 const { mainMenu, addDepartment, addRole, addEmployee } = require('./prompts');
+const db = require('./database');
 
 async function runApp() {
+    await db.initializeConnection();
+    
     let exit = false;
 
     while (!exit) {
@@ -8,15 +12,42 @@ async function runApp() {
 
         switch (action) {
             case 'View all departments':
-                // Logic to view all departments
+                const departments = await db.viewDepartments();
+                console.table(departments);
                 break;
+
             case 'Add a department':
                 const department = await addDepartment();
-                // Logic to add department to DB using department.departmentName
+                await db.addDepartment(department.departmentName);
+                console.log("Department added successfully!");
                 break;
-            // ... Handle other cases similarly
+
+            case 'View all roles':
+                const roles = await db.viewRoles();
+                console.table(roles);
+                break;
+
+            case 'Add a role':
+                const role = await addRole();
+                await db.addRole(role.roleName, role.salary, role.department);
+                console.log("Role added successfully!");
+                break;
+
+            case 'View all employees':
+                const employees = await db.viewEmployees();
+                console.table(employees);
+                break;
+
+            case 'Add an employee':
+                const employee = await addEmployee();
+                await db.addEmployee(employee.firstName, employee.lastName);
+                console.log("Employee added successfully!");
+                break;
+
             case 'Exit':
                 exit = true;
+                await db.closeConnection();
+                console.log("Goodbye!");
                 break;
         }
     }
